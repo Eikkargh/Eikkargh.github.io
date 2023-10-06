2f6a631db8e668e0d3e774d135d91c27dca7bf98
Eikkargh <87714048+Eikkargh@users.noreply.github.com>
2023-10-07 00:24:43 +0100

    Update afk.js

--- scripts/afk.js
+++ scripts/afk.js
@@ -47,7 +47,7 @@ function checkCookie() {
 			let key = keys[i];
 			let value = varSave[key];
 			if (value != null) {
-				document.getElementById(key).defaultValue = value;
+				document.getElementById(key).value = value;
 			}
 		}
 	}			
