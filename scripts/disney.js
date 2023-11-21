0fe9ed6d481643d08ee049c66ea06d3a5ae52bdc
Eikkargh <87714048+Eikkargh@users.noreply.github.com>
2023-11-21 22:56:03 +0000

    Update disney.js

--- scripts/disney.js
+++ scripts/disney.js
@@ -105,7 +105,6 @@ function checkCookie() {
 			let key = keys[i];
 			let value = varSave[key];
 			if (value != null && key != null) {
-				console.log("key:" + key + " value:" + value);
 				document.getElementById(key).value = value;
 			}
 		}
